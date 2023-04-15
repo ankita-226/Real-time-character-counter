f05b1ec65010b84bec797de885301ce530e3a27c
Ankita Yadav <ankitayaduvanshi226@gmail.com>
2023-04-16 02:25:21 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const charVal = document.getElementById("textarea");
+let totalCount = document.getElementById("total-counter");
+let remainingCount = document.getElementById("remaining-counter");
+let userChar = 0;
+const updateCounter = () => {
+ userChar = charVal.value.length;
+ totalCount.innerHTML =  userChar;
+ remainingCount.innerText =150 - userChar;
+}
+
+charVal.addEventListener("keyup", () => updateCounter());
+
+const copyText = () =>{
+charVal.select();
+charVal.setSelectionRange(0, 9999);
+navigator.clipboard.writeText(charVal.value)
+} 
